bf51626ecfe129e17569b55dfea056c208dbeaf0
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2018-11-13 15:01:21 -0600

    Add test

--- mdanalysissandbox/tests/test_mdanalysissandbox.py
+++ mdanalysissandbox/tests/test_mdanalysissandbox.py
@@ -4,12 +4,19 @@ Unit and regression test for the mdanalysissandbox package.
 
 # Import package, test suite, and other packages as needed
 import mdanalysissandbox
+from mdanalysissandbox import matt
 import pytest
 import sys
+
 
 def test_mdanalysissandbox_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mdanalysissandbox" in sys.modules
+
+
+def test_make_make():
+    test_cake = matt.make_cake()
+    assert test_cake == '🍰'
 
 
 def test_fixture(HVR):
